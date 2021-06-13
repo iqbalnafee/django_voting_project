401076b8d3bf9f94fef7d187b5b2d54a7578aa4d
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-06-13 16:06:04 +0600

    get data from db to html pahe shown is  done

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -8,5 +8,14 @@ from .models import Question, choice
 
 
 def index(request):
+
+    # first we need to get latest 5 questions from Question table
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    # now we need to make an object from this question_list
+    context = {"latest_question_list": latest_question_list}
+
+    # now we need to send this context to our template
+
     # return a template. polls/index.html is our template
-    return render(request, 'polls/index.html')
+    return render(request, 'polls/index.html', context)
