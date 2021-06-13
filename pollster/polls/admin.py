a719e0daa5914df4aeec5ccb91b9e7af410f720c
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-06-13 13:09:59 +0600

    choices under questions in admin side

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
+from .models import Question, choice
+
+# To show choices below Questions in admin side
+
+
+class ChoiceBelowQuestion(admin.TabularInline):
+    model = choice
+    extra = 3  # number of choices
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fieldsets consists of tuples
+    fieldsets = [
+
+        # tuple structure: first parameter is name of tuple,2nd param is an object with actual field name
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes':['collapse']})
+
+
+
+    ]
+    inlines = [ChoiceBelowQuestion]
+
+
 # Register your models here.
+
+admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Question)
+# admin.site.register(choice)
